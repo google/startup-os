c762c46e5a7f5636a56b0c346b814de3e591f7dd
Max Vorobev <vmax0770@gmail.com>
2018-04-16 21:58:09 +0300

    Fix license header and package name

--- tools/aa/Aa.java
+++ tools/aa/Aa.java
@@ -1,8 +1,24 @@
-package com.appstory.tools.aa;
+/*
+ * Copyright 2018 The StartupOS Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.appstory.tools.aa.commands.AaCommand;
-import com.appstory.tools.aa.commands.InitCommand;
-import com.appstory.tools.aa.Protos.Config;
+package com.google.startupos.tools.aa;
+
+import com.google.startupos.tools.aa.commands.AaCommand;
+import com.google.startupos.tools.aa.commands.InitCommand;
+import com.google.startupos.tools.aa.Protos.Config;
 import com.google.startupos.common.FileUtils;
 import com.google.startupos.common.flags.Flags;
 import java.io.IOException;
--- tools/aa/commands/InitCommand.java
+++ tools/aa/commands/InitCommand.java
@@ -1,7 +1,23 @@
-package com.appstory.tools.aa.commands;
+/*
+ * Copyright 2018 The StartupOS Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.appstory.tools.aa.Aa;
-import com.appstory.tools.aa.Protos.Config;
+package com.google.startupos.tools.aa.commands;
+
+import com.google.startupos.tools.aa.Aa;
+import com.google.startupos.tools.aa.Protos.Config;
 import com.google.startupos.common.FileUtils;
 import com.google.startupos.common.flags.Flag;
 import com.google.startupos.common.flags.FlagDesc;
