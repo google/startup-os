916405d1214fa4d896a47dac82836fda6d6f6af5
oferb <oferb@google.com>
2018-04-28 22:32:27 +0300

    Fix Dagger example imports

--- examples/dagger/example1/App.java
+++ examples/dagger/example1/App.java
@@ -16,8 +16,6 @@
 
 package com.google.startupos.examples.dagger.example1;
 
-import dagger.Component;
-
 public class App {
   public static void main(String[] args) {
     SomeComponent someComponent = DaggerSomeComponent.builder().build();
