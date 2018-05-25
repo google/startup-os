21a2bd72c186ff77458ca9a670ee0651c0515338
matemahinkhan <tutenstine@gmail.com>
2018-05-25 21:13:31 +0500

    misc fix and comment

--- tools/reviewer/web/src/app/dashboard/reviews-panel/review/editable-property/editable-property.component.ts
+++ tools/reviewer/web/src/app/dashboard/reviews-panel/review/editable-property/editable-property.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class EditablePropertyComponent {
   @Input() diff: Diff;
-  // Property name in Diff, e.g 'reviewers' or 'cc'.
+  // Property name in Diff, e.g 'bug' or 'description'.
   // The property value is stored in propertyValue
   @Input() property: string;
   @Input() editable: boolean = true;
