6abfe88aa9d669d18368810e265bf4558b14286d
oferb <oferb@google.com>
2018-06-14 23:26:51 +0300

    Fix

--- common/repo/tests/GitRepoTest.java
+++ common/repo/tests/GitRepoTest.java
@@ -1,13 +1,26 @@
+/*
+ * Copyright 2018 The StartupOS Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.startupos.common.repo.tests;
 
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import com.google.common.collect.ImmutableList;
 import com.google.startupos.common.CommonModule;
