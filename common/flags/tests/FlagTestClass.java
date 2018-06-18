953e42c56ef36ff9ab55131173222a7b7ae5ff75
val-fed <valerii.fedorenko.ua@gmail.com>
2018-06-18 16:31:27 +0300

    Make the code in Flags cleaner

--- common/flags/tests/FlagTestClass.java
+++ common/flags/tests/FlagTestClass.java
@@ -14,16 +14,16 @@
  * limitations under the License.
  */
 
-package com.google.startupos.common.flags.testpackage1;
+package com.google.startupos.common.flags.testpackage;
 
 import com.google.startupos.common.flags.Flag;
 import com.google.startupos.common.flags.FlagDesc;
 
-public class FlagTestClass {
+class FlagTestClass {
   @SuppressWarnings("unused")
   @FlagDesc(name = "string_test_flag", description = "A flag description")
-  public static final Flag<String> stringTestFlag = Flag.create("default value");
+  static final Flag<String> stringTestFlag = Flag.create("default value");
 
-  public class ClassWithoutFlags {
+  class ClassWithoutFlags {
   }
 }
