e94b88f85d7aaa49cf6423f8b6fb29f5541c2baa
matemahinkhan <tutenstine@gmail.com>
2018-06-29 09:02:46 +0500

    feat: reply popup html markup

--- tools/reviewer/web/src/app/dashboard/reviews-panel/index.ts
+++ tools/reviewer/web/src/app/dashboard/reviews-panel/index.ts
@@ -3,6 +3,7 @@ export * from './diff';
 export * from './review/person-list/person-list.component';
 export * from './review/editable-property/editable-property.component';
 export * from './review/review-titlebar/review-titlebar.component';
+export * from './review/review-titlebar/reply-popup/reply-popup.component';
 export * from './review/review-discussion/review-discussion.component';
 export * from './review/review-files/review-files.component';
 export * from './review/review.component';
---
+++ tools/reviewer/web/src/app/dashboard/reviews-panel/review/review-titlebar/reply-popup/reply-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cr-reply-popup',
+  templateUrl: './reply-popup.component.html',
+  styleUrls: ['./reply-popup.component.scss']
+})
+export class ReplyPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
