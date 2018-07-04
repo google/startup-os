2774c910aa7ae6c22d1b75fecc839edf277bcddb
Max Vorobev <vmax0770@gmail.com>
2018-07-04 19:55:32 +0300

    fix from review

--- tools/local_server/run.sh
+++ tools/local_server/run.sh
@@ -16,11 +16,12 @@ fi
 # Kill previous:
 if [ "$(uname)" = "Darwin" ] || [ "$(uname)" = "Linux" ]; then
   # Mac & Linux
-  # kill gateway (port 7000), gRPC server (port 8001) and Angular server (8000)
+  # kill HTTP gateway (port 7000), gRPC server (port 8001) and Angular server (8000)
   # -t makes `lsof` output only PIDs so output can be piped to `kill`
   # -n and -P prevent `lsof` from resolving addresses and ports, therefore
   # making execution faster
-  #
+  # we filter for processes that listen on port in order to kill only
+  # servers (i.e. Angular) instead of clients (i.e. Chrome)
   kill $(lsof -tnP -i:7000 -i:8000 -i:8001 -sTCP:LISTEN)
 # else
 #   # Windows
