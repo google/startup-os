34dc85377621bc325dce5e712f275179f4682f7a
Max Vorobev <vmax0770@gmail.com>
2018-07-29 11:06:51 +0200

    Whitelist for Maven dependencies and WORKSPACE (#184)
    * Whitelist for Maven dependencies
    
    * WORKSPACE whitelist
    
    * fix package name
    
    * Fix whitelisting implementation
    
    * latest fixes from review
    
    * fixes from review
    
    * fixes from review
    
    * fix whitespace.yaml
    
    * latest fixes from review
    
    * latest fix from review

--- WORKSPACE
+++ WORKSPACE
@@ -60,13 +60,11 @@ grpc_java_repositories(
 )
 
 # Google Maven Repository
-GMAVEN_TAG = "20180513-1"
-
 http_archive(
     name = "gmaven_rules",
     sha256 = "da44017f6d7bc5148a73cfd9bf8dbb1ee5a1301a596edad9181c5dc7648076ae",
-    strip_prefix = "gmaven_rules-%s" % GMAVEN_TAG,
-    url = "https://github.com/bazelbuild/gmaven_rules/archive/%s.tar.gz" % GMAVEN_TAG,
+    strip_prefix = "gmaven_rules-20180513-1",
+    url = "https://github.com/bazelbuild/gmaven_rules/archive/20180513-1.tar.gz",
 )
 
 load("@gmaven_rules//:gmaven.bzl", "gmaven_rules")
@@ -219,10 +217,7 @@ http_archive(
     urls = ["https://github.com/bazelbuild/rules_docker/archive/v0.4.0.tar.gz"],
 )
 
-load(
-    "@io_bazel_rules_docker//java:image.bzl",
-    _java_image_repos = "repositories",
-)
+load("@io_bazel_rules_docker//java:image.bzl", _java_image_repos = "repositories")
 
 _java_image_repos()
 
---
+++ tools/dep_whitelist/WorkspaceDepsWhitelistTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2018 The StartupOS Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.startupos.tools.dep_whitelist;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+
+/*
+  Test verifies whether all external dependencies
+  (Maven, git_repository, http_file) all whitelisted
+  (contained in list for key `workspace_dependencies`
+  in whitelist.yaml)
+
+  To simplify the test, we make the following assumptions:
+  - '://' appears only once in each line
+  - '://' always appears in the same line as '='
+*/
+public class WorkspaceDepsWhitelistTest {
+  private Map<String, List<String>> whitelist;
+  private List<String> workspace;
+  private static Set<String> keysToValidate =
+      new HashSet<String>() {
+        {
+          add("url");
+          add("urls");
+          add("remote");
+          add("artifact");
+        }
+      };
+
+  // Trim quotes, spaces, array brackets and trailing commas
+  // from parsed value
+  private static final String TRIM_PATTERN = "[\\s\\[\\]\\,\"]";
+
+  @Before
+  public void setUp() throws Exception {
+    whitelist = new Yaml().load(new FileInputStream(new File("whitelist.yaml")));
+    try (Stream<String> stream = Files.lines(Paths.get("WORKSPACE"))) {
+      workspace = stream.collect(Collectors.toList());
+    }
+  }
+
+  @Test
+  public void urlsMatchWhitelist() throws Exception {
+    List<String> validUrls = whitelist.get("workspace_dependencies");
+
+    for (String line : workspace) {
+      if (line.contains("=")) {
+        String[] kv = line.split("=");
+        assertTrue(String.format("Line %s should contain key and value", line), kv.length == 2);
+        assertTrue(
+            String.format("Line %s should not contain :// more than once", line),
+            StringUtils.countMatches(line, "://") < 2);
+        boolean isValidUrl = false;
+
+        String key = kv[0].trim();
+        String value = kv[1].replaceAll(TRIM_PATTERN, "");
+        if (keysToValidate.contains(key)) {
+
+          for (String validUrl : validUrls) {
+            if (value.startsWith(validUrl)) {
+              isValidUrl = true;
+              break;
+            }
+          }
+
+          assertTrue(String.format("URL %s is not in the whitelist", value), isValidUrl);
+        } else {
+          assertFalse(
+              String.format("Value at key %s should not contain ://", key), value.contains("://"));
+        }
+      } else {
+        assertFalse(
+            String.format("Line %s should not contain ://", line),
+            !line.startsWith("#") && line.contains("://"));
+      }
+    }
+  }
+}
+
