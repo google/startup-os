a4c1ab49830d3ec7fedcd76c2b79b1c98deae1a5
Ofer Bartal <oferb@google.com>
2018-09-08 00:36:15 +0300

    Move aa and local_server to reviewer

---
+++ tools/reviewer/aa/commands/SyncCommand.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2018 The StartupOS Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.startupos.tools.reviewer.aa.commands;
+
+import com.google.startupos.common.FileUtils;
+import com.google.startupos.common.repo.GitRepo;
+import com.google.startupos.common.repo.GitRepoFactory;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+public class SyncCommand implements AaCommand {
+  private static final String TEMP_BRANCH_FOR_SYNC = "temp_branch_for_sync";
+
+  private final FileUtils fileUtils;
+  private GitRepoFactory repoFactory;
+  private String headPath;
+  private String workspaceName;
+  private String workspacePath;
+  private Map<String, String> repoToInitialBranch = new HashMap<>();
+
+  @Inject
+  public SyncCommand(
+      FileUtils utils,
+      GitRepoFactory repoFactory,
+      @Named("Head path") String headPath,
+      @Named("Workspace name") String workspaceName,
+      @Named("Workspace path") String workspacePath) {
+    this.fileUtils = utils;
+    this.repoFactory = repoFactory;
+    this.headPath = headPath;
+    this.workspaceName = workspaceName;
+    this.workspacePath = workspacePath;
+  }
+
+  @Override
+  public boolean run(String[] args) {
+    // Pull all repos in head
+    try {
+      fileUtils
+          .listContents(headPath)
+          .stream()
+          .map(path -> fileUtils.joinToAbsolutePath(headPath, path))
+          .filter(fileUtils::folderExists)
+          .forEach(
+              path -> {
+                System.out.println(String.format("[HEAD]: Performing sync: %s", path));
+                repoFactory.create(path).pull();
+              });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    // then, do the sync for all workspaces
+    try {
+      fileUtils
+          .listContents(workspacePath)
+          .stream()
+          .map(path -> fileUtils.joinToAbsolutePath(workspacePath, path))
+          .filter(fileUtils::folderExists)
+          .forEach(
+              path -> {
+                String repoName = Paths.get(path).getFileName().toString();
+                System.out.println(
+                    String.format("[%s/%s]: Performing sync", workspaceName, repoName));
+                GitRepo repo = repoFactory.create(path);
+                repoToInitialBranch.put(repoName, repo.currentBranch());
+                if (repo.branchExists(TEMP_BRANCH_FOR_SYNC)) {
+                  repo.removeBranch(TEMP_BRANCH_FOR_SYNC);
+                }
+                System.out.println(
+                    String.format("[%s/%s]: switching to temp branch", workspaceName, repoName));
+                repo.switchBranch(TEMP_BRANCH_FOR_SYNC);
+                System.out.println(
+                    String.format("[%s/%s]: committing all changes", workspaceName, repoName));
+                repo.commit(repo.getUncommittedFiles(), "Sync: temporary commit");
+                System.out.println(
+                    String.format("[%s/%s]: switching to master", workspaceName, repoName));
+                repo.switchBranch("master");
+                System.out.println(String.format("[%s/%s]: pulling", workspaceName, repoName));
+                repo.pull();
+                System.out.println(
+                    String.format("[%s/%s]: merging changes", workspaceName, repoName));
+                boolean mergeResult = repo.merge("temp_branch_for_sync");
+                if (!mergeResult) {
+                  System.out.println(
+                      String.format(
+                          "[%s/%s]: manual merge required, check files for conflicts",
+                          workspaceName, repoName));
+                }
+                System.out.println(
+                    String.format("[%s/%s]: removing temp branch", workspaceName, repoName));
+                repo.removeBranch("temp_branch_for_sync");
+              });
+    } catch (Exception e) {
+      revertChanges(repoToInitialBranch);
+      e.printStackTrace();
+    }
+    return true;
+  }
+
+  private void revertChanges(Map<String, String> repoToInitialBranch) {
+    if (!repoToInitialBranch.isEmpty()) {
+      repoToInitialBranch.forEach(
+          (repoName, initialBranch) -> {
+            GitRepo repo =
+                repoFactory.create(fileUtils.joinToAbsolutePath(workspacePath, repoName));
+            String currentBranch = repo.currentBranch();
+            if (!currentBranch.equals(initialBranch)) {
+              repo.switchBranch(initialBranch);
+            }
+          });
+    }
+  }
+}
+
---
+++ tools/reviewer/aa/commands/WorkspaceCommand.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2018 The StartupOS Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.startupos.tools.reviewer.aa.commands;
+
+import com.google.startupos.common.FileUtils;
+import com.google.startupos.common.flags.Flag;
+import com.google.startupos.common.flags.FlagDesc;
+import com.google.startupos.common.flags.Flags;
+import com.google.startupos.tools.reviewer.aa.Protos.Config;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+/**
+ * This command is used to switch between workspaces and create new ones.
+ *
+ * <p>Usage: To switch to a workspace: aa workspace <workspace name>
+ *
+ * <p>To create and then switch to a workspace: aa workspace -f <workspace name>
+ *
+ * <p>To remove workspace (should exist and not be active): aa workspace -r <workspace name>
+ */
+// TODO: If there's only one repo in the workspace, cd should enter into it.
+// TODO: If there's multiple repos in the workspace, and I'm currently in a repo in a workspace,
+// cd should enter into that repo.
+public class WorkspaceCommand implements AaCommand {
+  @FlagDesc(name = "force", description = "Create workspace if it doesn't exist")
+  public static Flag<Boolean> force = Flag.create(false);
+
+  @FlagDesc(name = "remove", description = "Remove workspace if it exists")
+  public static Flag<Boolean> remove = Flag.create(false);
+
+  private final FileUtils fileUtils;
+  private final Config config;
+
+  // set by processArgs
+  private ActionMode mode;
+  private String workspacePath;
+
+  @Inject
+  public WorkspaceCommand(FileUtils fileUtils, Config config) {
+    this.fileUtils = fileUtils;
+    this.config = config;
+  }
+
+  private enum ActionMode {
+    CREATE, // aaw -f ws_name
+    SWITCH, // aaw ws_name
+    REMOVE // aaw -r ws_name
+  }
+
+  private boolean removeWorkspace() {
+    Path currentPath = Paths.get("").toAbsolutePath();
+    if (!fileUtils.folderExists(workspacePath)) {
+      System.err.println(RED_ERROR + "Workspace does not exist");
+      return false;
+    }
+
+    if (currentPath.startsWith(workspacePath)) {
+      System.err.println(RED_ERROR + "Trying to remove active workspace");
+      return false;
+    }
+    fileUtils.deleteDirectoryUnchecked(workspacePath);
+    return true;
+  }
+
+  private boolean createWorkspace() {
+    if (fileUtils.folderExists(workspacePath)) {
+      System.err.println(RED_ERROR + "Workspace already exists");
+      return false;
+    } else {
+      fileUtils.mkdirs(workspacePath);
+      try {
+        fileUtils.copyDirectoryToDirectory(
+            fileUtils.joinToAbsolutePath(config.getBasePath(), "head"),
+            workspacePath,
+            "^bazel-.*$",
+            "node_modules");
+      } catch (IOException e) {
+        fileUtils.deleteDirectoryUnchecked(workspacePath);
+        e.printStackTrace();
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean switchWorkspace() {
+    if (!fileUtils.folderExists(workspacePath)) {
+      System.err.println(RED_ERROR + "Workspace does not exist");
+      return false;
+    }
+
+    String guessedRepoFolder = guessFolderForCd(workspacePath);
+    if (guessedRepoFolder != null) {
+      System.out.println(String.format("cd %s", guessedRepoFolder));
+    } else {
+      System.out.println(String.format("cd %s", workspacePath));
+    }
+    return true;
+  }
+
+  private boolean processArgs(String[] args) {
+    // The Flags library does not support short flags by design, as this increases the chance of
+    // flag collisions between packages. To allow short flags here, we do a flag replacement.
+    for (int i = 0; i < args.length; i++) {
+      if (args[i].equals("-f")) {
+        args[i] = "--force";
+      } else if (args[i].equals("-r")) {
+        args[i] = "--remove";
+      }
+    }
+    if (args.length < 2) {
+      System.err.println(RED_ERROR + "Missing workspace name");
+      return false;
+    }
+
+    String workspaceName = args[args.length - 1];
+    workspacePath = fileUtils.joinToAbsolutePath(config.getBasePath(), "ws", workspaceName);
+
+    if (workspaceName.startsWith("-")) {
+      System.err.println(RED_ERROR + "Missing workspace name");
+      return false;
+    }
+
+    args = Arrays.copyOfRange(args, 0, args.length - 1);
+    Flags.parseCurrentPackage(args);
+
+    if (force.get() && remove.get()) {
+      System.err.println(RED_ERROR + "Cannot specify both force and remove flags");
+      return false;
+    }
+
+    if (force.get()) {
+      mode = ActionMode.CREATE;
+    } else if (remove.get()) {
+      mode = ActionMode.REMOVE;
+    } else {
+      mode = ActionMode.SWITCH;
+    }
+
+    return true;
+  }
+
+  // Guess folder to cd into after workspace command
+  private String guessFolderForCd(String workspacePath) {
+    // Try using current workspace subfolder
+    String guessedRepoFolder =
+        fileUtils.joinToAbsolutePath(workspacePath, fileUtils.getCurrentWorkingDirectoryName());
+    if (fileUtils.folderExists(guessedRepoFolder)) {
+      return guessedRepoFolder;
+    }
+    // If target workspace has only 1 subfolder, use it
+    try {
+      if (fileUtils.listContents(workspacePath).size() == 1) {
+        guessedRepoFolder =
+            fileUtils.joinToAbsolutePath(
+                workspacePath, fileUtils.listContents(workspacePath).get(0));
+        if (fileUtils.folderExists(guessedRepoFolder)) {
+          return guessedRepoFolder;
+        }
+      }
+    } catch (Exception e) {
+      // Fail silently. It's just a nice-to-have.
+    }
+    return null;
+  }
+
+  @Override
+  public boolean run(String[] args) {
+    // Note: System.out gets executed by the calling aa_tool.sh to run commands such as cd.
+    if (!processArgs(args)) {
+      return false;
+    }
+
+    if (mode == ActionMode.CREATE) {
+      if (createWorkspace()) {
+        switchWorkspace();
+      } else {
+        return false;
+      }
+    } else if (mode == ActionMode.REMOVE) {
+      return removeWorkspace();
+    } else {
+      switchWorkspace();
+    }
+
+    return true;
+  }
+}
+
