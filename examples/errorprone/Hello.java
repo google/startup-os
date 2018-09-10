0393d909c34571abc1933320845d40c1205d2411
Max Vorobev <vmax0770@gmail.com>
2018-09-10 13:18:16 +0300

    Integrate error-prone custom check (#250)
    * Integrate error-prone custom check
    
    * update bazel_deps binary to fix a bug with removing third_party/maven/BUILD
    
    * adapt dependency proto to new proto to fix CI
    
    * fix PrintStream in SuggestedFix; remove startupos_binaries from WORKSPACE
    
    * document steps on error-prone checks
    
    * import proto message directly in error-prone example

---
+++ examples/errorprone/Hello.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 The StartupOS Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.startupos.examples.errorprone;
+
+// <editor-fold desc="Uncomment to test ProtobufCheck">
+// import com.google.startupos.proto_vs_json.Protos.Person;
+// </editor-fold>
+
+public class Hello {
+  public static void main(String[] args) {
+
+    // <editor-fold desc="Uncomment to test StringFmtInPrintMethodsCheck">
+    // System.err.print(String.format("Hello: %s\n", "World"));
+    // </editor-fold>
+
+    // <editor-fold desc="Uncomment to test ProtobufCheck">
+    // Person.newBuilder().build();
+    // </editor-fold>
+  }
+}
+
