caf7576d1c912f1ee74b076fb3d2831d085a78ce
Ofer Bartal <oferb@google.com>
2018-09-15 14:02:31 +0300

    Create global registry for Reviewer instances

---
+++ tools/reviewer/GlobalRegistryTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 The StartupOS Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.startupos.tools.reviewer;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.collect.ImmutableList;
+import com.google.startupos.common.CommonModule;
+import com.google.startupos.common.FileUtils;
+import dagger.Component;
+import dagger.Provides;
+import java.nio.file.FileSystem;
+import javax.inject.Singleton;
+import org.junit.Before;
+import org.junit.Test;
+
+/* A test to check global_registry.prototxt and global_registry.protobin match */
+public class GlobalRegistryTest {
+
+  @Singleton
+  @Component(modules = {CommonModule.class, AaModule.class})
+  interface TestComponent {
+    FileUtils getFileUtils();
+  }
+
+  private FileUtils fileUtils;
+
+  @Before
+  public void setup() {
+    fileUtils = component.getFileUtils();
+  }
+
+  @Test
+  public void protoFileTest() throws Exception {
+    // TODO: Test that .reviewer/global_registry.prototxt and .reviewer/global_registry.protobin
+    // match. Print matching protobin if not.
+  }
+}
+
