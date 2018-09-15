f3f4da85f92576c080be6392d00353d5d4d2a744
Valerii Fedorenko <valerii.fedorenko.ua@gmail.com>
2018-09-15 09:45:47 +0300

    Add rename proto fields tutorial (#270)
    * Create tools for reading and writing protobin
    
    * Delete PersonWriter
    
    * Add renaming-proto.md tutorial
    
    * Add the license
    
    * Add `Open in Google Cloud Shell` button to README.md
    
    * Add PersonWriter
    
    * Change renaming-proto.md
    
    * Fix renaming-proto.md
    
    * Second fixing of renaming-proto.md
    
    * Third fixing of renaming-proto.md
    
    * Fix formatting: make lines shorter in renaming-proto.md
    
    * Small fixes
    
    * Simplify tutorial
    
    * Add changes according to review
    
    * Change `Renaming message field` part
    
    * Add `Renaming a message` item to tutorial and small fixes

---
+++ tutorials/proto_rename/PersonTool.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018 The StartupOS Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.startupos.tutorials.protorename;
+
+import com.google.startupos.common.CommonModule;
+import com.google.startupos.common.FileUtils;
+import com.google.startupos.tutorials.protorename.Protos.Person;
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+public class PersonTool {
+  private static String personProtobinaryPath;
+  private static FileUtils fileUtils;
+
+  public static void main(String[] args) {
+    fileUtils = DaggerPersonTool_PersonToolComponent.create().getFileUtils();
+    personProtobinaryPath =
+        fileUtils.joinToAbsolutePath(fileUtils.getCurrentWorkingDirectory(), "person.protobin");
+
+    if (!processArgs(args)) {
+      System.exit(1);
+    }
+
+    if (args[0].equals("write")) {
+      writePerson();
+    } else {
+      readPerson();
+    }
+  }
+
+  private static void writePerson() {
+    Person person =
+        Person.newBuilder()
+            .setName("John")
+            .setId(1)
+            .setFavoritePizzaTopping(Person.FavoritePizzaTopping.OLIVES_AND_PINEAPLE)
+            .build();
+
+    fileUtils.writeProtoBinaryUnchecked(person, personProtobinaryPath);
+  }
+
+  private static void readPerson() {
+    System.out.println(
+        fileUtils.readProtoBinaryUnchecked(personProtobinaryPath, Person.newBuilder()));
+  }
+
+  private static boolean processArgs(String[] args) {
+    if (args.length == 0) {
+      System.out.println(
+          "Missing input arg. Please use `read` arg for reading or `write` for writing.");
+      return false;
+    } else if (!args[0].equals("read") && !args[0].equals("write")) {
+      System.out.println(
+          "The input arg is incorrect. Please use `read` arg for reading or `write` for writing.");
+      return false;
+    }
+    return true;
+  }
+
+  @Singleton
+  @Component(modules = {CommonModule.class})
+  interface PersonToolComponent {
+    FileUtils getFileUtils();
+  }
+}
+
