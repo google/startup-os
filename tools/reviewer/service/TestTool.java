0a43867ba5f2c40dad3234de5e16655c211c7963
Web Developland <webdevelopland@protonmail.com>
2018-11-24 04:21:43 +0300

    commit 6

--- tools/reviewer/service/TestTool.java
+++ tools/reviewer/service/TestTool.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    https://www.apache.org/licenses/LICENSE-2.0
+ *    Hi to all Reviewer testers! :D
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
