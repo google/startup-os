a9596d0e07045661959c1552a7048f5724512835
Max Vorobev <vmax0770@gmail.com>
2018-12-23 21:12:06 +0300

    Follow-up commit to fixing bazel_tools

--- tools/bazel_tools/buildifier.sh
+++ tools/bazel_tools/buildifier.sh
@@ -7,7 +7,7 @@ platform=$(uname)
 if [[ "$platform" == "Darwin" ]]; then
     BINARY=$(find . -iwholename "*buildifier_osx/file/downloaded" | head -n1)
 elif [[ "$platform" == "Linux" ]]; then
-    BINARY=find . -iwholename "*buildifier/file/downloaded" | head -n1
+    BINARY=$(find . -iwholename "*buildifier/file/downloaded" | head -n1)
 else
     echo "Buildifier does not have a binary for $platform"
     exit 1
--- tools/bazel_tools/unused_deps.sh
+++ tools/bazel_tools/unused_deps.sh
@@ -7,7 +7,7 @@ platform=$(uname)
 if [[ "$platform" == "Darwin" ]]; then
     BINARY=$(find . -iwholename "*unused_deps_osx/file/downloaded" | head -n1)
 elif [[ "$platform" == "Linux" ]]; then
-    BINARY=find . -iwholename "*unused_deps/file/downloaded" | head -n1
+    BINARY=$(find . -iwholename "*unused_deps/file/downloaded" | head -n1)
 else
     echo "unused_deps does not have a binary for $platform"
     exit 1
