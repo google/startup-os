9afc03051b9df966102544ff88ff8f7744d332f5
Max Vorobev <vmax0770@gmail.com>
2018-12-30 21:05:25 +0300

    fix //tools/deps:MavenDepsWhitelistTest-checkstyle

--- tools/deps/MavenDepsWhitelistTest.java
+++ tools/deps/MavenDepsWhitelistTest.java
@@ -16,20 +16,19 @@
 
 package com.google.startupos.tools.dep_whitelist;
 
-import org.junit.Test;
-import org.junit.Before;
-import java.util.Map;
-import java.util.Set;
-import java.util.List;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.FileInputStream;
-import java.io.File;
-
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
 import org.yaml.snakeyaml.Yaml;
+
 
 public class MavenDepsWhitelistTest {
   private Map<String, Map<String, Object>> dependencies;
