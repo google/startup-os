85d6a23e0cb24a8456027eab78a345adf20d95c0
val-fed <valerii.fedorenko.ua@gmail.com>
2019-01-03 22:18:46 +0200

    Save third_party_deps.prototxt file as zip-archive

--- tools/build_file_generator/ThirdPartyDepsTool.java
+++ tools/build_file_generator/ThirdPartyDepsTool.java
@@ -18,39 +18,61 @@ package com.google.startupos.tools.buildfilegenerator;
 
 import com.google.startupos.common.CommonModule;
 import com.google.startupos.common.FileUtils;
-import com.google.startupos.common.flags.Flag;
-import com.google.startupos.common.flags.FlagDesc;
-import com.google.startupos.common.flags.Flags;
+import com.google.startupos.tools.buildfilegenerator.Protos.ThirdPartyDeps;
 import dagger.Component;
 
 import javax.inject.Singleton;
 import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
 
-import com.google.startupos.tools.buildfilegenerator.Protos.ThirdPartyDeps;
-
-/* A tool for generating prototxt file to know which classes each third_party dependency includes
- *
- * Note! Before using this tool, it is necessary to run `./build.sh`
- * to have all third_party deps in `bazel-startup-os/external` folder
+/* A tool for generating the zip-archive with prototxt file inside
+ * to know which classes each third_party dependency includes.
+ * Path to the zip-archive: "//tools/build_file_generator/third_party_deps.zip"
  *
  * Usage:
- * bazel run //tools/build_file_generator:third_party_deps_tool -- --file_path </path/to/file>
+ * bazel run //tools/build_file_generator:third_party_deps_tool
  */
 public class ThirdPartyDepsTool {
-  @FlagDesc(name = "file_path", description = "Absolute path to file")
-  private static Flag<String> filePath = Flag.create("");
-
   public static void main(String[] args) throws IOException {
-    Flags.parseCurrentPackage(args);
-    checkInputFlags();
-
     ThirdPartyDepsToolComponent component =
         DaggerThirdPartyDepsTool_ThirdPartyDepsToolComponent.create();
 
-    ThirdPartyDeps thirdPartyDeps = component.getThirdPartyDepsAnalyzer().getThirdPartyDeps();
+    createZip(component.getFileUtils(), component.getThirdPartyDepsAnalyzer().getThirdPartyDeps());
+  }
 
-    component.getFileUtils().writePrototxtUnchecked(thirdPartyDeps, filePath.get());
+  private static void createZip(FileUtils fileUtils, ThirdPartyDeps thirdPartyDeps)
+      throws IOException {
+    final String prototxtFile = "third_party_deps.prototxt";
+    String absBuildFileGeneratorDirPath =
+        fileUtils.joinPaths(fileUtils.getCurrentWorkingDirectory(), "tools/build_file_generator");
+
+    Map<String, String> env = new HashMap<>();
+    env.put("create", "true");
+
+    URI uri =
+        URI.create(
+            String.format(
+                "jar:file:%s",
+                fileUtils.joinPaths(absBuildFileGeneratorDirPath, "third_party_deps.zip")));
+    try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
+      fileUtils.writePrototxtUnchecked(
+          thirdPartyDeps, fileUtils.joinPaths(absBuildFileGeneratorDirPath, prototxtFile));
+      Path prototxtFilePath =
+          Paths.get(fileUtils.joinPaths(absBuildFileGeneratorDirPath, prototxtFile));
+      Files.copy(
+          prototxtFilePath, zipfs.getPath("/" + prototxtFile), StandardCopyOption.REPLACE_EXISTING);
+    } finally {
+      fileUtils.deleteFileOrDirectoryIfExists(
+          fileUtils.joinPaths(absBuildFileGeneratorDirPath, prototxtFile));
+    }
   }
 
   @Singleton
@@ -59,16 +81,6 @@ public class ThirdPartyDepsTool {
     FileUtils getFileUtils();
 
     ThirdPartyDepsAnalyzer getThirdPartyDepsAnalyzer();
-  }
-
-  private static void checkInputFlags() {
-    if (filePath.get().isEmpty()) {
-      throw new IllegalArgumentException(
-          "`file_path` flag should contain absolute path to file, but is empty");
-    } else if (!Paths.get(filePath.get()).isAbsolute()) {
-      throw new IllegalArgumentException(
-          "`file_path` flag should contain absolute path to file, but: " + filePath.get());
-    }
   }
 }
 
