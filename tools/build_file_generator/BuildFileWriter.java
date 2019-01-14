1adbe188c4478818e101bf5c0d744f315f1e7af0
val-fed <valerii.fedorenko.ua@gmail.com>
2019-01-14 16:01:01 +0200

    Add support checkstyle_test and java_grpc_library

--- tools/build_file_generator/BuildFileWriter.java
+++ tools/build_file_generator/BuildFileWriter.java
@@ -16,12 +16,16 @@
 
 package com.google.startupos.tools.buildfilegenerator;
 
+import com.google.common.flogger.FluentLogger;
 import com.google.startupos.common.FileUtils;
 import com.google.startupos.tools.buildfilegenerator.Protos.BuildFile;
+import com.google.startupos.tools.buildfilegenerator.Protos.BuildFile.CheckstyleTestExtension;
 import com.google.startupos.tools.buildfilegenerator.Protos.BuildFile.JavaBinary;
+import com.google.startupos.tools.buildfilegenerator.Protos.BuildFile.JavaGrpcLibrary;
 import com.google.startupos.tools.buildfilegenerator.Protos.BuildFile.JavaLibrary;
 import com.google.startupos.tools.buildfilegenerator.Protos.BuildFile.JavaProtoLibrary;
 import com.google.startupos.tools.buildfilegenerator.Protos.BuildFile.JavaTest;
+import com.google.startupos.tools.buildfilegenerator.Protos.BuildFile.LoadExtensionStatement;
 import com.google.startupos.tools.buildfilegenerator.Protos.BuildFile.ProtoLibrary;
 
 import java.util.List;
@@ -30,10 +34,12 @@ import javax.inject.Inject;
 /* This class saves a BuildFile proto to a BUILD file. If a BUILD file exists
 and is not auto-generated, it writes to BUILD.generated. */
 public class BuildFileWriter {
+  private static final FluentLogger log = FluentLogger.forEnclosingClass();
   private static final String HEADER = "# THIS FILE IS AUTO-GENERATED";
   private static final String REGULAR_FILENAME = "BUILD";
   private static final String GENERATED_FILENAME = "BUILD.generated";
-
+  private static final String PACKAGE_DEFINITION =
+      "package(default_visibility = [\"//visibility:public\"])";
   private final FileUtils fileUtils;
 
   @Inject
@@ -43,15 +49,21 @@ public class BuildFileWriter {
 
   public void write(BuildFile buildFile, String packagePath) {
     StringBuilder result = new StringBuilder();
+    List<CheckstyleTestExtension> checkstyleTests = buildFile.getCheckstyleTestList();
     result
         .append(HEADER)
         .append(System.lineSeparator())
         .append(System.lineSeparator())
+        .append(PACKAGE_DEFINITION)
+        .append(System.lineSeparator())
+        .append(System.lineSeparator())
+        .append(getLoadExtensions(buildFile.getExtensionList()))
         .append(getProtoLibraries(buildFile.getProtoLibraryList()))
         .append(getJavaProtoLibraries(buildFile.getJavaProtoLibraryList()))
-        .append(getJavaLibraries(buildFile.getJavaLibraryList()))
-        .append(getJavaBinaries(buildFile.getJavaBinaryList()))
-        .append(getJavaTests(buildFile.getJavaTestList()));
+        .append(getJavaGrpcLibraries(buildFile.getJavaGrpcLibraryList()))
+        .append(getJavaLibraries(buildFile.getJavaLibraryList(), checkstyleTests))
+        .append(getJavaBinaries(buildFile.getJavaBinaryList(), checkstyleTests))
+        .append(getJavaTests(buildFile.getJavaTestList(), checkstyleTests));
 
     String buildFilePath = fileUtils.joinPaths(packagePath, "BUILD");
     if (!fileUtils.fileExists(buildFilePath)
@@ -62,74 +74,16 @@ public class BuildFileWriter {
     }
   }
 
-  private String getJavaTests(List<JavaTest> javaTests) {
+  private String getLoadExtensions(List<LoadExtensionStatement> loadExtensions) {
     StringBuilder result = new StringBuilder();
-    for (JavaTest javaTest : javaTests) {
+    for (LoadExtensionStatement loadExtension : loadExtensions) {
       result
-          .append("java_test(")
-          .append(System.lineSeparator())
-          .append(String.format("  name = \"%s\",", javaTest.getName()))
-          .append(getArgument("srcs", javaTest.getSrcsList()))
-          .append(System.lineSeparator())
-          .append(String.format("  test_class = \"%s\",", javaTest.getTestClass()))
-          .append(getArgument("deps", javaTest.getDepsList()))
-          .append(System.lineSeparator())
-          .append(")")
-          .append(System.lineSeparator())
+          .append(
+              String.format(
+                  "load(\"%s\", \"%s\")", loadExtension.getPath(), loadExtension.getSymbol()))
           .append(System.lineSeparator());
     }
-    return result.toString();
-  }
-
-  private String getJavaBinaries(List<JavaBinary> javaBinaries) {
-    StringBuilder result = new StringBuilder();
-    for (JavaBinary javaBinary : javaBinaries) {
-      result
-          .append("java_binary(")
-          .append(System.lineSeparator())
-          .append(String.format("  name = \"%s\",", javaBinary.getName()))
-          .append(getArgument("srcs", javaBinary.getSrcsList()))
-          .append(System.lineSeparator())
-          .append(String.format("  main_class = \"%s\",", javaBinary.getMainClass()))
-          .append(getArgument("deps", javaBinary.getDepsList()))
-          .append(System.lineSeparator())
-          .append(")")
-          .append(System.lineSeparator())
-          .append(System.lineSeparator());
-    }
-    return result.toString();
-  }
-
-  private String getJavaLibraries(List<JavaLibrary> javaLibraries) {
-    StringBuilder result = new StringBuilder();
-    for (JavaLibrary javaLibrary : javaLibraries) {
-      result
-          .append("java_library(")
-          .append(System.lineSeparator())
-          .append(String.format("  name = \"%s\",", javaLibrary.getName()))
-          .append(getArgument("srcs", javaLibrary.getSrcsList()))
-          .append(getArgument("deps", javaLibrary.getDepsList()))
-          .append(System.lineSeparator())
-          .append(")")
-          .append(System.lineSeparator())
-          .append(System.lineSeparator());
-    }
-    return result.toString();
-  }
-
-  private String getJavaProtoLibraries(List<JavaProtoLibrary> javaProtoLibraries) {
-    StringBuilder result = new StringBuilder();
-    for (JavaProtoLibrary javaProtoLibrary : javaProtoLibraries) {
-      result
-          .append("java_proto_library(")
-          .append(System.lineSeparator())
-          .append(String.format("  name = \"%s\",", javaProtoLibrary.getName()))
-          .append(getArgument("deps", javaProtoLibrary.getDepsList()))
-          .append(System.lineSeparator())
-          .append(")")
-          .append(System.lineSeparator())
-          .append(System.lineSeparator());
-    }
+    result.append(System.lineSeparator());
     return result.toString();
   }
 
@@ -167,11 +121,130 @@ public class BuildFileWriter {
     return result.toString();
   }
 
+  private String getJavaProtoLibraries(List<JavaProtoLibrary> javaProtoLibraries) {
+    StringBuilder result = new StringBuilder();
+    for (JavaProtoLibrary javaProtoLibrary : javaProtoLibraries) {
+      result
+          .append("java_proto_library(")
+          .append(System.lineSeparator())
+          .append(String.format("  name = \"%s\",", javaProtoLibrary.getName()))
+          .append(getArgument("deps", javaProtoLibrary.getDepsList()))
+          .append(System.lineSeparator())
+          .append(")")
+          .append(System.lineSeparator())
+          .append(System.lineSeparator());
+    }
+    return result.toString();
+  }
+
+  private String getJavaGrpcLibraries(List<JavaGrpcLibrary> javaGrpcLibraries) {
+    StringBuilder result = new StringBuilder();
+    for (JavaGrpcLibrary grpcLibrary : javaGrpcLibraries) {
+      result
+          .append("java_grpc_library(")
+          .append(System.lineSeparator())
+          .append(String.format("  name = \"%s\",", grpcLibrary.getName()))
+          .append(getArgument("srcs", grpcLibrary.getSrcsList()))
+          .append(getArgument("tags", grpcLibrary.getTagsList()))
+          .append(getArgument("deps", grpcLibrary.getDepsList()))
+          .append(System.lineSeparator())
+          .append(")")
+          .append(System.lineSeparator())
+          .append(System.lineSeparator());
+    }
+    return result.toString();
+  }
+
+  private String getJavaLibraries(
+      List<JavaLibrary> javaLibraries, List<CheckstyleTestExtension> checkstyleTests) {
+    StringBuilder result = new StringBuilder();
+    for (JavaLibrary javaLibrary : javaLibraries) {
+      result
+          .append("java_library(")
+          .append(System.lineSeparator())
+          .append(String.format("  name = \"%s\",", javaLibrary.getName()))
+          .append(getArgument("srcs", javaLibrary.getSrcsList()))
+          .append(getArgument("deps", javaLibrary.getDepsList()))
+          .append(System.lineSeparator())
+          .append(")")
+          .append(System.lineSeparator())
+          .append(System.lineSeparator());
+      result.append(getCheckstyleTest(checkstyleTests, javaLibrary.getName()));
+    }
+    return result.toString();
+  }
+
+  private String getJavaBinaries(
+      List<JavaBinary> javaBinaries, List<CheckstyleTestExtension> checkstyleTests) {
+    StringBuilder result = new StringBuilder();
+    for (JavaBinary javaBinary : javaBinaries) {
+      result
+          .append("java_binary(")
+          .append(System.lineSeparator())
+          .append(String.format("  name = \"%s\",", javaBinary.getName()))
+          .append(getArgument("srcs", javaBinary.getSrcsList()))
+          .append(System.lineSeparator())
+          .append(String.format("  main_class = \"%s\",", javaBinary.getMainClass()))
+          .append(getArgument("deps", javaBinary.getDepsList()))
+          .append(System.lineSeparator())
+          .append(")")
+          .append(System.lineSeparator())
+          .append(System.lineSeparator());
+      result.append(getCheckstyleTest(checkstyleTests, javaBinary.getName()));
+    }
+    return result.toString();
+  }
+
+  private String getJavaTests(
+      List<JavaTest> javaTests, List<CheckstyleTestExtension> checkstyleTests) {
+    StringBuilder result = new StringBuilder();
+    for (JavaTest javaTest : javaTests) {
+      result
+          .append("java_test(")
+          .append(System.lineSeparator())
+          .append(String.format("  name = \"%s\",", javaTest.getName()))
+          .append(getArgument("srcs", javaTest.getSrcsList()))
+          .append(getArgument("resources", javaTest.getResourcesList()))
+          .append(System.lineSeparator())
+          .append(String.format("  test_class = \"%s\",", javaTest.getTestClass()))
+          .append(getArgument("deps", javaTest.getDepsList()))
+          .append(System.lineSeparator())
+          .append(")")
+          .append(System.lineSeparator())
+          .append(System.lineSeparator());
+      result.append(getCheckstyleTest(checkstyleTests, javaTest.getName()));
+    }
+    return result.toString();
+  }
+
+  //  resources = ["resources/test_proto.proto"],
+
+  private String getCheckstyleTest(List<CheckstyleTestExtension> checkstyleTests, String target) {
+    StringBuilder result = new StringBuilder();
+    for (CheckstyleTestExtension checkstyleTest : checkstyleTests) {
+      if (checkstyleTest.getTarget().replace(":", "").equals(target)) {
+        result
+            .append("checkstyle_test(")
+            .append(System.lineSeparator())
+            .append(String.format("  name = \"%s\",", checkstyleTest.getName()))
+            .append(System.lineSeparator())
+            .append(String.format("  target = \"%s\",", checkstyleTest.getTarget()))
+            .append(System.lineSeparator())
+            .append(")")
+            .append(System.lineSeparator())
+            .append(System.lineSeparator());
+        return result.toString();
+      }
+    }
+    log.atWarning().log("Can't find the checkstyle_test for %s target", target);
+    return result.toString();
+  }
+
   private String getArgument(String argumentName, List<String> labels) {
     StringBuilder result = new StringBuilder();
     if (!labels.isEmpty()) {
       result.append(System.lineSeparator());
-      result.append("  ").append(argumentName).append("= [");
+      result.append("  ").append(argumentName).append(" = [");
       if (labels.size() == 1) {
         result.append(String.format("\"%s\"],", labels.get(0)));
       } else {
