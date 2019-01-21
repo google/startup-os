a0ad09e5e5a620306c9f94657b89de4a0323066c
Max Vorobev <vmax0770@gmail.com>
2019-01-21 10:38:25 +0300

    fix android sdk downloading script

--- tools/get_android_sdk.sh
+++ tools/get_android_sdk.sh
@@ -3,16 +3,16 @@
 RED=$(tput setaf 1)
 RESET=$(tput sgr0)
 
-if [ -z "$ANDROID_HOME" ]; then
+if [[ -z "$ANDROID_HOME" ]]; then
   export ANDROID_HOME=$HOME/android_sdk/
 fi
 
 DOWNLOAD_COMMAND=wget
-if [ ! -x "$(command -v $DOWNLOAD_COMMAND)" ]; then
+if [[ ! -x "$(command -v ${DOWNLOAD_COMMAND})" ]]; then
   DOWNLOAD_COMMAND="curl -LO"
 fi
 
-if [ ! -x "$(command -v $DOWNLOAD_COMMAND)" ]; then
+if [[ ! -x "$(command -v ${DOWNLOAD_COMMAND})" ]]; then
   echo "$(tput setaf 1)No wget or curl found$(tput sgr0)"
   exit 1
 fi
@@ -20,10 +20,10 @@ fi
 URL_BASE="https://dl.google.com/android/repository/"
 
 platform=$(uname)
-if [ "$platform" == "Darwin" ]; then
+if [[ "$platform" == "Darwin" ]]; then
   FILENAME="sdk-tools-darwin-4333796.zip"
   PROFILE_FILE="$HOME/.bash_profile"
-elif [ "$platform" == "Linux" ]; then
+elif [[ "$platform" == "Linux" ]]; then
   FILENAME="sdk-tools-linux-4333796.zip"
   PROFILE_FILE="$HOME/.bashrc"
 else
@@ -31,12 +31,15 @@ else
   exit 1
 fi
 
-if [ ! -d "$ANDROID_HOME" ]; then
-  $DOWNLOAD_COMMAND $URL_BASE/$FILENAME
-  unzip $FILENAME -d $ANDROID_HOME
-  (yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses) || true
-  $ANDROID_HOME/tools/bin/sdkmanager "platforms;android-27" "build-tools;27.0.3"
-  echo "Removing $(rm -v $FILENAME)"
+if [[ ! -d "$ANDROID_HOME" ]]; then
+  ${DOWNLOAD_COMMAND} ${URL_BASE}/${FILENAME}
+  unzip ${FILENAME} -d ${ANDROID_HOME}
+  echo y | ${ANDROID_HOME}/tools/bin/sdkmanager "platforms;android-27"
+  echo y | ${ANDROID_HOME}/tools/bin/sdkmanager "platform-tools"
+  echo y | ${ANDROID_HOME}/tools/bin/sdkmanager "build-tools;27.0.3"
+  echo y | ${ANDROID_HOME}/tools/bin/sdkmanager "extras;android;m2repository"
+  echo y | ${ANDROID_HOME}/tools/bin/sdkmanager "extras;google;m2repository"
+  echo "Removing $(rm -v ${FILENAME})"
 fi
 
 echo "Android SDK at $RED$ANDROID_HOME$RESET"
