4c44d9e3eff52ce3ff2436b63983529674e02233
Vadim <webdevelopland@protonmail.com>
2019-02-05 14:57:22 +0300

    Reviewer webapp - file expand update (#520)

--- tools/reviewer/webapp/src/app/dashboard/diff/diff-files/diff-files.component.ts
+++ tools/reviewer/webapp/src/app/dashboard/diff/diff-files/diff-files.component.ts
@@ -1,9 +1,20 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Diff, File, Thread } from '@/core/proto';
 import { TextDiffReturn, TextDiffService } from '@/core/services';
 import { Section } from '@/shared/code-changes';
+
+interface ChangeFile {
+  filename: string;
+  isExpanded: boolean;
+  data?: TextDiffReturn;
+  sections?: Section[];
+}
+
+interface ChangeFileMap {
+  [filename: string]: ChangeFile;
+}
 
 // The component implements UI of file list of the diff
 // How it looks: https://i.imgur.com/8vZfGTc.jpg
@@ -12,70 +23,122 @@ import { Section } from '@/shared/code-changes';
   templateUrl: './diff-files.component.html',
   styleUrls: ['./diff-files.component.scss'],
 })
-export class DiffFilesComponent implements OnChanges {
+export class DiffFilesComponent implements OnInit, OnChanges {
   threads: Thread[];
   isExpanded: boolean = false;
-  isCodeLoading: boolean = false;
-  changes: TextDiffReturn[];
-  // Sections for each file
-  sectionsList: Section[][] = [];
+  isLoading: boolean = false;
+  changeFileMap: ChangeFileMap = {};
 
   @Input() diff: Diff;
   @Input() files: File[];
 
   constructor(private textDiffService: TextDiffService) { }
 
+  ngOnInit() {
+    this.createChangeFileMap();
+  }
+
   ngOnChanges(ngChanges: SimpleChanges) {
-    if (this.changes && ngChanges.diff) {
-      this.checkExpand();
+    // Update changes when new diff is received from firebase
+    if (this.isExpanded && ngChanges.diff) {
+      this.loadAndExpandAllFiles();
     }
+  }
+
+  // Creates collapsed file list
+  private createChangeFileMap(): void {
+    for (const file of this.files) {
+      const filename: string = file.getFilenameWithRepo();
+      this.changeFileMap[filename] = {
+        filename: filename,
+        isExpanded: false,
+      };
+    }
+  }
+
+  getChangeFiles(): ChangeFile[] {
+    return Object.values(this.changeFileMap);
+  }
+
+  // Is at least one file expanded?
+  private getExpand(): boolean {
+    let isExpanded: boolean = false;
+    for (const changeFile of Object.values(this.changeFileMap)) {
+      if (changeFile.isExpanded) {
+        isExpanded = true;
+        break;
+      }
+    }
+    return isExpanded;
   }
 
   // Expands or collapses all files
   toggleExpand(): void {
-    if (this.isCodeLoading) {
+    if (this.isExpanded) {
+      for (const changeFile of Object.values(this.changeFileMap)) {
+        changeFile.isExpanded = false;
+        changeFile.sections = undefined;
+      }
+      this.isExpanded = false;
+    } else {
+      this.loadAndExpandAllFiles();
+    }
+  }
+
+  // Expands or collapses specific file
+  toggleFileExpand(changeFile: ChangeFile): void {
+    if (!changeFile.isExpanded) {
+      this.textDiffService.load(this.diff, changeFile.filename)
+        .subscribe(textDiffReturn => {
+          changeFile.data = textDiffReturn;
+          changeFile.isExpanded = true;
+          this.isExpanded = true;
+        });
+    } else {
+      changeFile.sections = undefined;
+      changeFile.isExpanded = false;
+      if (!this.getExpand()) {
+        this.isExpanded = false;
+      }
+    }
+  }
+
+  saveSections(sections: Section[], changeFile: ChangeFile): void {
+    changeFile.sections = sections;
+  }
+
+  private loadAndExpandAllFiles(): void {
+    if (this.isLoading) {
       return;
     }
 
-    this.isExpanded = !this.isExpanded;
-    if (!this.isExpanded) {
-      this.sectionsList = [];
+    this.isLoading = true;
+
+    // Create subscribers to synchronize all loadings
+    const subscribers = [];
+    for (const file of this.files) {
+      const subscriber: Observable<TextDiffReturn> = this.textDiffService.load(
+        this.diff,
+        file.getFilenameWithRepo(),
+      );
+      subscribers.push(subscriber);
     }
-    this.checkExpand();
-  }
 
-  saveSections(sections: Section[], changesIndex: number): void {
-    this.sectionsList[changesIndex] = sections;
-  }
-
-  // Start changes loading, if files are expanded
-  private checkExpand(): void {
-    if (this.isExpanded) {
-      this.isCodeLoading = true;
-      this.loadChanges().subscribe(() => {
-        this.isCodeLoading = false;
+    // Load all files
+    Observable
+      .zip(...subscribers)
+      .subscribe((textDiffReturns: TextDiffReturn[]) => {
+        for (const textDiffReturn of textDiffReturns) {
+          const filename: string = textDiffReturn.leftFile.getFilenameWithRepo();
+          this.changeFileMap[filename] = {
+            filename: filename,
+            data: textDiffReturn,
+            sections: this.changeFileMap[filename].sections,
+            isExpanded: true,
+          };
+        }
+        this.isExpanded = true;
+        this.isLoading = false;
       });
-    }
-  }
-
-  // Loads TextDiffReturn from firebase
-  private loadChanges(): Observable<void> {
-    return new Observable(observer => {
-      const subscribers = [];
-      for (const file of this.files) {
-        const subscriber: Observable<TextDiffReturn> = this.textDiffService.load(
-          this.diff,
-          file.getFilenameWithRepo(),
-        );
-        subscribers.push(subscriber);
-      }
-
-      Observable
-        .zip(...subscribers)
-        .subscribe((textDiffReturns: TextDiffReturn[]) => {
-          this.changes = textDiffReturns;
-          observer.next();
-        });
-    });
   }
 }
