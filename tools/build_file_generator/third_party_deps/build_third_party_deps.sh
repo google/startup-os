5c319e5c0fe98f7bf23e3f0c86c18ff5d3323c40
val-fed <valerii.fedorenko.ua@gmail.com>
2019-03-10 08:45:12 +0200

    Add script to build third party deps

---
+++ tools/build_file_generator/third_party_deps/build_third_party_deps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+bazel build //third_party/...
+
+echo "Third party dependencies were successfully built."
--- tools/build_file_generator/third_party_deps/update_third_party_deps_zip.sh
+++ tools/build_file_generator/third_party_deps/update_third_party_deps_zip.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
-readonly repo_path=$(git rev-parse --show-toplevel)
-readonly build_command="$repo_path/tools/build_or_test.sh build"
 readonly ZIPNAME=third_party_deps.zip
 
-# We need to build whole project to have all dependencies in `bazel-<repo_name>/external` folder
+# We need to have all third party dependencies in `bazel-<repo_name>/external` folder
 # before creating `third_party_deps.zip`
-$build_command && ./create_third_party_deps_zip.sh
+./build_third_party_deps.sh && ./create_third_party_deps_zip.sh
 
 echo "$ZIPNAME was successfully updated"
