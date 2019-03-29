b14e68322f0b3ac00c5c0fd3b3e14e83643a7524
val-fed <valerii.fedorenko.ua@gmail.com>
2019-03-29 10:50:45 +0200

    Make the script portability

--- tools/build_file_generator/third_party_deps/third_party_deps_updater.sh
+++ tools/build_file_generator/third_party_deps/third_party_deps_updater.sh
@@ -7,18 +7,17 @@ readonly REPO_ABS_PATH=$(git rev-parse --show-toplevel)
 readonly BUILD_FILE_ABS_PATH="$REPO_ABS_PATH/tools/build_file_generator/third_party_deps/BUILD"
 
 # Getting public third-party target names
-third_party_folder_path="$REPO_ABS_PATH/third_party"
 actual_third_party_targets=()
 for entry in $(bazel query 'attr("visibility", "//visibility:public", //third_party/...)'); do
   actual_third_party_targets+=("$entry")
 done
 
-# Deleting old targets
-sed -i '/# %START%/,/# %END%/{//!d;}' ${BUILD_FILE_ABS_PATH}
+# Deleting text between %START% and %END%, while preserving the markers
+sed -i.bak '/# %START%/,/# %END%/{//!d;}' ${BUILD_FILE_ABS_PATH} && rm ${BUILD_FILE_ABS_PATH}.bak
 
-# Adding new targets
 for target_to_add in "${actual_third_party_targets[@]}"; do
-  sed -i "/# %START%/a \"${target_to_add}\"," ${BUILD_FILE_ABS_PATH}
+  # Adding target_to_add as next line under %START% marker
+  sed -i.bak "/# %START%/a \"${target_to_add}\"," ${BUILD_FILE_ABS_PATH} && rm ${BUILD_FILE_ABS_PATH}.bak
 done
 
 echo "Third party targets are updated"
