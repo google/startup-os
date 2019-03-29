05cd41a5e301cb2e29d09257c98b995bf0b0566c
Max Vorobev <vmax0770@gmail.com>
2019-03-29 07:58:04 +0200

    Apply @vmax suggestions from code review
    Co-Authored-By: val-fed <valerii.fedorenko.ua@gmail.com>

--- tools/build_file_generator/third_party_deps/third_party_deps_updater.sh
+++ tools/build_file_generator/third_party_deps/third_party_deps_updater.sh
@@ -8,13 +8,13 @@ readonly BUILD_FILE_ABS_PATH="$REPO_ABS_PATH/tools/build_file_generator/third_pa
 
 # Getting public third-party target names
 third_party_folder_path="$REPO_ABS_PATH/third_party"
-declare -a actual_third_party_targets
+actual_third_party_targets=()
 for entry in $(bazel query 'attr("visibility", "//visibility:public", //third_party/...)'); do
-  actual_third_party_targets=("${actual_third_party_targets[@]}" "$entry")
+  actual_third_party_targets+=("$entry")
 done
 
 # Deleting old targets
-sed -i '/# %START%/,/# %END%/{//!d}' ${BUILD_FILE_ABS_PATH}
+sed -i '/# %START%/,/# %END%/{//!d;}' ${BUILD_FILE_ABS_PATH}
 
 # Adding new targets
 for target_to_add in "${actual_third_party_targets[@]}"; do
